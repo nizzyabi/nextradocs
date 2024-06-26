d699396e90688c83d95dd1fd051999113f738e89
dpaulos6 <itzframepvp@outlook.com>
2024-06-26 17:54:16 +0100

    main page improvement

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -4,28 +4,25 @@ import "./index.module.css";
 
 export default function MainPage(): JSX.Element {
   return (
-    <section className="w-full flex-1 py-32 overflow-hidden">
+    <section className="w-full min-h-[60vh] flex-1 py-24 lg:py-32 overflow-hidden">
       <div className="flex flex-col items-center space-y-10 xl:space-y-16">
-        <div className="grid gap-4 px-10 md:grid-cols-2 md:gap-16 max-w-6xl">
-          <div className="flex flex-col items-start gap-4">
-            <h2 className="m-0">Welcome to the Nizzy Starter Docs!</h2>
-            <span className="mx-auto max-w-[700px] text-muted-foreground md:text-xl">
-              This docs contain a series of guides with examples for libraries
-              and tools I use on all my projects. New content will be
-              continuously added, so feel free to suggest guides or
-              improvements!
-            </span>
-            <div className="space-x-4">
-              <Link
-                href="/docs/intro"
-                className="inline-flex text-white hover:text-white hover:no-underline h-9 items-center justify-center rounded-md bg-primary hover:bg-primary-dark px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-              >
-                Read Docs
-              </Link>
-            </div>
+        <div className="flex flex-col items-start gap-6 px-10 max-w-6xl">
+          <h2 className="m-0">Welcome to the Nizzy Starter Docs!</h2>
+          <span className="mx-auto max-w-[700px] text-muted-foreground md:text-xl">
+            This docs contain a series of guides with examples for libraries and
+            tools I use on all my projects. New content will be continuously
+            added, so feel free to suggest guides or improvements!
+          </span>
+          <div className="space-x-4">
+            <Link
+              href="/docs/intro"
+              className="inline-flex text-white hover:text-white hover:no-underline h-9 items-center justify-center rounded-md bg-primary hover:bg-primary-dark px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+            >
+              Read Docs
+            </Link>
           </div>
         </div>
-        <div className="max-w-7xl mx-auto w-full flex-grow absolute top-[45%] left-1/2 -translate-x-1/2">
+        <div className="max-w-7xl mx-auto w-full flex-grow md:absolute md:top-[45%] md:left-1/2 md:-translate-x-1/2">
           <div className="relative text-foreground opacity-5">
             <div className="w-full absolute top-0 left-0 h-full bg-gradient-to-b from-background z-10 pointer-events-none"></div>
             <QrCode className="absolute top-12 md:top-1/4 left-1/2 -translate-x-1/2 w-72 lg:w-96 h-auto hover:text-primary transition" />
